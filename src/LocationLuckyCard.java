939bea27fdb682b0f0e864b003aa25cf2d7cabe8
Snickersxs <alperenbayar2@gmail.com>
2018-11-16 02:32:30 +0300

    Fixed/implemented some more TODOs  Implemented Lucky Card functions.

--- src/LocationLuckyCard.java
+++ src/LocationLuckyCard.java
@@ -6,10 +6,12 @@ public class LocationLuckyCard extends Location {
     }
 
     public static void imFeelingLucky(Player player) {
-        int ret = (int)((Math.random() * 2));
+        int ret = (int)((Math.random() * 3));
         switch (ret) {
             case 0:
-                // TODO call function to add/remove cash from Player's current cash
+                int random1 = (int )(Math.random() * 400 + 100);
+                System.out.println("You take a lucky card and your cash decrease "+random1);
+                player.setCash(player.getCash()+random1);
                 // quantity will be randomly selected
                 break;
             case 1:
@@ -17,8 +19,27 @@ public class LocationLuckyCard extends Location {
                 player.setInJail(true);
                 break;
             case 2:
-                // TODO call function to move Player lcoation by random number
+                int random = (int)((Math.random() * 10));
+                if(random == 5){
+                    while (random==5){
+                        random = (int)((Math.random() * 10));
+                    }
+                }
+                System.out.println("You take a lucky card and your location changed by  :" );//TODO gelen random indexin sehir ismini yazdir
+                player.setCurrLocationIndex(random);
                 break;
+
+            case 3:
+
+                int random2 = (int )(Math.random() * 400 + 100);
+                System.out.println("You take a lucky card and your cash decrease "+random2);
+                if((player.getCash()-random2)<0){
+                    player.setCash(0);
+                }
+                else {
+                    player.setCash(player.getCash() - random2);
+                }
+
             default:
                 //
                 break;
