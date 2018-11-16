dd2eef496f316a5aa6b3c54373e3c0cc40eedf7d
sirmordred <oguzhan97yigit@gmail.com>
2018-11-16 12:44:50 +0300

    More and more updates and fixes  *We are now saving jail and luckycard's locations so we can now have multiple jails and luckycards  *Added some more TODOs

--- src/LocationJail.java
+++ src/LocationJail.java
@@ -4,10 +4,16 @@ import java.util.List;
 public class LocationJail extends Location{
 
     // All instances of Jail will share the same players in jail
-    private static List<Player> playersInJail = new ArrayList<>();
+    private static List<Player> playersInJail = new ArrayList<>(); // TODO delete if it is unused/unimportant but keep it for now
+
+    public static List<Integer> getJailLocations() {
+        return jailLocations;
+    }
+
+    private static List<Integer> jailLocations = new ArrayList<>();
 
     public LocationJail(String name, int locationIndex){
         super(name, locationIndex);
+        jailLocations.add(locationIndex); // Save multiple jail's location in one instance(static)
     }
-
 }
--- src/LocationLuckyCard.java
+++ src/LocationLuckyCard.java
@@ -1,22 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class LocationLuckyCard extends Location {
 
+    private static int prices[] = {-500,-400,-300,-200,-100,100,200,300,400,500};
+    private static List<Integer> luckyCardLocations = new ArrayList<>(); // Save multiple luckycard's location in one instance(static)
 
     public LocationLuckyCard(String name, int locationIndex) {
         super(name, locationIndex);
+        luckyCardLocations.add(locationIndex);
+    }
+
+    public static List<Integer> getLuckyCardLocations() {
+        return luckyCardLocations;
     }
 
     public static void imFeelingLucky(Player player) {
-        int ret = (int)((Math.random() * 3));
+        int ret = (int)((Math.random() * 2));
         switch (ret) {
             case 0:
-                int random1 = (int )(Math.random() * 400 + 100);
-                System.out.println("You take a lucky card and your cash decrease "+random1);
-                player.setCash(player.getCash()+random1);
-                // quantity will be randomly selected
+                int rndIndex = new Random().nextInt(prices.length);
+                int rndPrice = prices[rndIndex];
+                if(rndPrice < 0) {
+                    System.out.println("You take a lucky card and your cash has been decreased by "
+                            + rndPrice + "$");
+                } else {
+                    System.out.println("You take a lucky card and your cash has been increased by "
+                            + rndPrice + "$");
+                }
+                player.setCash(player.getCash()+rndPrice);
                 break;
             case 1:
                 System.out.println("Lucky card : You are in jail now.");
                 player.setInJail(true);
+                // TODO also set player's location to JAIL's location
                 break;
             case 2:
                 int random = (int)((Math.random() * 10));
@@ -27,8 +45,9 @@ public class LocationLuckyCard extends Location {
                 }
                 System.out.println("You take a lucky card and your location changed by  :" );//TODO gelen random indexin sehir ismini yazdir
                 player.setCurrLocationIndex(random);
+                // TODO tekrar luckycard'a veya hapise gelebilir ona göre ayarlamalar yap
                 break;
-
+/* TODO disabled for now (olasılıkların balansını ayarla)
             case 3:
 
                 int random2 = (int )(Math.random() * 400 + 100);
@@ -39,9 +58,8 @@ public class LocationLuckyCard extends Location {
                 else {
                     player.setCash(player.getCash() - random2);
                 }
-
+*/
             default:
-                //
                 break;
         }
     }
