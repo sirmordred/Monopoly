723626ea40a5eb4726331b295e59679f749f1cce
Oğuzhan Yiğit <oguzhan97yigit@gmail.com>
2018-11-26 17:35:30 +0000

    Update and rename LocationLuckyCardTest.java to TestLocationLuckyCard.java

---
+++ test/TestLocationLuckyCard.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestLocationLuckyCard {
+
+    @Test
+    public void testImFeelingLucky() {
+        // TODO implemented corresponding test
+    }
+
+}
